ef9380f9b578a4e2c9a0a1044f15c3338da03d5d
Алмаз Миннибаев <almaz200@bk.ru>
2022-12-06 23:30:34 +0300

    create_tg_bot

--- homework.py
+++ homework.py
@@ -1,16 +1,26 @@
-...
+import logging
+import os
+
+import requests
+
+import telegram
+from datetime import time
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, Updater
+from pprint import pprint
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = 'y0_AgAAAABI78NEAAYckQAAAADV5LaO1Ay2TPtIQxm2HzNBdCXnPK14P48'
+TELEGRAM_TOKEN = '5553314893:AAGml8iWXCeja37cjdvWjChn5jAPOBM0ELs'
+TELEGRAM_CHAT_ID = 561180852
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
+timestamp = {'from_date': 1664917200}
 
 HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
@@ -20,23 +30,27 @@ HOMEWORK_VERDICTS = {
 
 
 def check_tokens():
-    ...
+    """Проверяет доступность переменных окружения,
+    которые необходимы для работы программы."""
 
+    return all((PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID))
 
 def send_message(bot, message):
-    ...
+    return bot.send_message(TELEGRAM_CHAT_ID, message)
 
 
 def get_api_answer(timestamp):
-    ...
+    homework_statuses = requests.get(ENDPOINT, headers=HEADERS, params=timestamp)
+    response = requests.get(ENDPOINT).json()
 
 
 def check_response(response):
-    ...
+    response = requests.get('https://api.github.com')
+
 
 
 def parse_status(homework):
-    ...
+    pass
 
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
@@ -61,6 +75,7 @@ def main():
             ...
         ...
 
-
+    
 if __name__ == '__main__':
     main()
+    
